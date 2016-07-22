9ae9f3fd5e2234082f1457ada408c5b22e23962a
David Konsumer <konsumer@jetboystudio.com>
2016-07-22 10:59:49 -0700

    I like wordpress theme, better

--- utils/typography.js
+++ utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from 'typography'
-import funstonTheme from 'typography-theme-funston'
+import Wordpress2016 from 'typography-theme-wordpress-2016'
 
-const typography = new Typography(funstonTheme)
+const typography = new Typography(Wordpress2016)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== 'production') {
