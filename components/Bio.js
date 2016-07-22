06e78ef746cec3ca1a22c4c490ee518471c6b748
David Konsumer <konsumer@jetboystudio.com>
2016-07-22 10:48:31 -0700

    better require for image

--- components/Bio.js
+++ components/Bio.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { config } from 'config'
 import { rhythm } from 'utils/typography'
 import { prefixLink } from 'gatsby-helpers'
-import profilePic from '../profile-pic.png'
+import profilePic from 'profile-pic.png'
 
 const Bio = props => (
   <p style={{  marginBottom: rhythm(2.5)}}>
