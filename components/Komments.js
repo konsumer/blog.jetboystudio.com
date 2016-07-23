f007ce6488bf2a94e383af6633c781987a525c2f
David Konsumer <konsumer@jetboystudio.com>
2016-07-22 18:10:39 -0700

    komment support

---
+++ components/Komments.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default class Komments extends React.Component {
+  render () {
+    setTimeout(() => {
+      const t = document.createElement('script')
+      t.type = 'text/javascript'
+      t.async = !0
+      t.src = `//komments.net/embed/${this.props.code}`
+      this.refs.kmt.appendChild(t)
+    }, 0)
+    return (<div ref="kmt" id="_kmts"></div>)
+  }
+}
+
+Komments.propTypes = {
+  code: React.PropTypes.string
+}
