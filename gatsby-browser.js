287725f3181fd4fcf0fed011731c9b89ece0cd99
David Konsumer <konsumer@jetboystudio.com>
2017-05-27 13:47:06 -0700

    global location

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -10,7 +10,7 @@ exports.onRouteUpdate = state => {
   if (config.gaCode) {
     const page = find(pages, {path: state.pathname})
     ReactGA.ga('send', 'pageview', {
-      location: location.pathname,
+      location: window.location.pathname,
       title: page && page.data && page.data.title ? page.data.title : state.pathname,
       page: state.pathname
     })
